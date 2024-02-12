4867ea298fa8e4538e782c06e7c1a4a927e16b9a
alPumpur <aleftinapumpur@gmail.com>
2024-02-12 22:54:15 +0700

    Реализована частично логика заполнения карточек

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,7 +3,7 @@ const app = new Vue({
     data: {
         column1: [
             {
-                title: 'Card 1',
+                title: 'List 1',
                 items: [
                     { text: 'Item 1', completed: false },
                     { text: 'Item 2', completed: false },
@@ -13,7 +13,7 @@ const app = new Vue({
         ],
         column2: [
             {
-                title: 'Card 2',
+                title: 'List 2',
                 items: [
                     { text: 'Item 1', completed: false },
                     { text: 'Item 2', completed: false },
@@ -25,5 +25,29 @@ const app = new Vue({
         ],
         column3: []
     },
+    methods: {
+        checkCompletion(card) {
+            const totalItems = card.items.length;
+            const completedItems = card.items.filter(item => item.completed).length;
+            const completionPercentage = (completedItems / totalItems) * 100;
 
+            if (completionPercentage >= 50 && completionPercentage < 100) {
+                if (!this.column2.find(c => c === card)) {
+                    this.column2.push(card);
+                    const index = this.column1.findIndex(c => c === card);
+                    this.column1.splice(index, 1);
+                }
+            } else if (completionPercentage === 100) {
+                const index = this.column2.findIndex(c => c === card);
+                if (index !== -1) {
+                    this.column2.splice(index, 1);
+                }
+                this.column3.push({
+                    title: card.title,
+                    items: card.items,
+                    completed: true,
+                });
+            }
+        }
+    }
 });
